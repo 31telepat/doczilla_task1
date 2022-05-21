01d83c5958340993e0138af1c7304d45b4a98204
DESKTOP-TP2AU31\Пользователь <max31.86@yandex.ru>
2022-05-21 21:31:45 +0300

    add sorting and writting content to result file

--- src/com/company/StartApplication.java
+++ src/com/company/StartApplication.java
@@ -1,24 +1,48 @@
 package com.company;
 
-import java.io.IOException;
+import java.io.*;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.Collections;
+import java.util.Comparator;
 
 public class StartApplication {
 
     public static void main(String[] args) throws IOException {
 
+        //create folder and result file if not exist
         CreateFile createFile = new CreateFile();
         createFile.createFile();
 
+        //select folder to find files
         Path source = Paths.get("D:\\IT\\testfolder");
 
+        //select texted files
         MyFileVisitor visitor = new MyFileVisitor();
         Files.walkFileTree(source, visitor);
-        for (Path listPath : visitor.getListPathTextFiles())
-        {
-            System.out.println(listPath);
+
+        //sort path by name
+        Collections.sort(visitor.getListPathTextFiles(), new Comparator<Path>() {
+            @Override
+            public int compare(Path o1, Path o2) {
+                Path namePath1 = o1.getFileName();
+                Path namePath2 = o2.getFileName();
+                return namePath1.compareTo(namePath2);
+            }
+        });
+
+        //write file content to result file
+        BufferedWriter writer = new BufferedWriter(new FileWriter(createFile.getPathOfResultFile().toFile()));
+        for (Path path : visitor.getListPathTextFiles()){
+            BufferedReader reader = new BufferedReader(new FileReader(String.valueOf(path)));
+
+            while (reader.ready()){
+                writer.write(reader.readLine());
+                writer.newLine();
+            }
+            reader.close();
         }
+        writer.close();
     }
 }
